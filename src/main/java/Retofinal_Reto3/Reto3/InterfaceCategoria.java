cac97aa37774a7c742039270fdc886d1bf107508
javierflet <javierflet@hotmail.com>
2021-11-17 06:47:25 -0500

    repositorio correcto
    Repositorio final actualizado

---
+++ src/main/java/Retofinal_Reto3/Reto3/InterfaceCategoria.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Retofinal_Reto3.Reto3;
+
+/**
+ *
+ * @author mac
+ */
+import org.springframework.data.repository.CrudRepository;
+public interface InterfaceCategoria extends CrudRepository<Categoria,Integer> {
+    
+} 
---
+++ src/main/java/Retofinal_Reto3/Reto3/InterfaceCliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Retofinal_Reto3.Reto3;
+
+/**
+ *
+ * @author mac
+ */
+
+import org.springframework.data.repository.CrudRepository;
+public interface InterfaceCliente extends CrudRepository<Cliente,Integer> {
+    
+} 
